f5fd074dd8e0d2ad39150bae0effcdf35cd967de
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-02-25 23:35:16 -0300

    Formulário e Hot reload

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Jogo:
@@ -8,16 +8,32 @@ class Jogo:
         self.console = console
 
 
+jogo1 = Jogo(nome='Tetris', categoria='Puzzle', console='Atari')
+jogo2 = Jogo(nome='God of War', categoria='Rack n Slash', console='PS2')
+jogo3 = Jogo(nome='Mortal Kombat', categoria='Luta', console='PS2')
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
 
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo(nome='Tetris', categoria='Puzzle', console='Atari')
-    jogo2 = Jogo(nome='God of War', categoria='Rack n Slash', console='PS2')
-    jogo3 = Jogo(nome='Mortal Kombat', categoria='Luta', console='PS2')
-    lista = [jogo1, jogo2, jogo3]
+@app.route('/')
+def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
-app.run()
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo jogo')
+
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=lista)
+
+
+app.run(debug=True)
