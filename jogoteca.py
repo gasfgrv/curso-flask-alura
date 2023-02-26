22efe5b76818e05c0545f4a5a195130bd3650e56
Gustavo Silva <gustavo_almeida11@hotmail.com>
2023-02-25 22:57:25 -0300

    Código orientado a objetos

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,22 @@
 from flask import Flask, render_template
 
+
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
 app = Flask(__name__)
 
 
 @app.route('/inicio')
 def ola():
-    lista = ['Tetris', 'Skyrim', 'Crash']
+    jogo1 = Jogo(nome='Tetris', categoria='Puzzle', console='Atari')
+    jogo2 = Jogo(nome='God of War', categoria='Rack n Slash', console='PS2')
+    jogo3 = Jogo(nome='Mortal Kombat', categoria='Luta', console='PS2')
+    lista = [jogo1, jogo2, jogo3]
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
 
